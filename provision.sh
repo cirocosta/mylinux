a1681ec19d1e047dec54e3316a76a1532b2070df
Ciro S. Costa <ciroscosta@vmware.com>
2021-09-01 20:47:07 +0000

    bump
    Signed-off-by: Ciro S. Costa <ciroscosta@vmware.com>

--- provision.sh
+++ provision.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-
 # installs what's necessary :p
 #
 # ps.: assumes you're a regular user (not `root`).
 #
 
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-
-main () {
-	setup_bashrc
-	setup_gitconfig
-	install_apt_deps
-	install_go
-	install_autojump
+main() {
+        setup_bashrc
+        setup_gitconfig
+        install_apt_deps
+        install_go
+        install_autojump
         setup_vim
         setup_tmux
 }
 
-install_apt_deps () {
+install_apt_deps() {
         sudo apt update
         sudo apt install -y \
-		bzip2 \
+                bzip2 \
                 bash-completion \
                 build-essential \
                 curl \
@@ -45,29 +42,27 @@ install_apt_deps () {
                 vim
 }
 
-install_go () {
+install_go() {
         sudo chown -R $(whoami) /usr/local
         curl -SL https://golang.org/dl/go1.17.linux-amd64.tar.gz | tar xvzf - -C /usr/local
 
         echo "
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
-        " >> ~/.bashrc
+        " >>~/.bashrc
 }
 
-
-setup_vim () {
+setup_vim() {
         git clone --recursive https://github.com/cirocosta/dot-vim ~/.vim
         ln -s $(realpath ~/.vim/.vimrc) $(realpath ~/.vimrc)
 
         echo "
 export VISUAL=vim
 export EDITOR=vim
-        " >> ~/.bashrc
+        " >>~/.bashrc
 }
 
-
-install_autojump () {
+install_autojump() {
         local deb_file=/tmp/jump.deb
 
         curl -o $deb_file -SL https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_0.40.0_amd64.deb
@@ -75,9 +70,8 @@ install_autojump () {
         rm $deb_file
 }
 
-
-setup_gitconfig () {
-        cat << 'EOF' > ~/.gitconfig
+setup_gitconfig() {
+        cat <<'EOF' >~/.gitconfig
 [alias]
         ci = commit -s
 [push]
@@ -87,9 +81,8 @@ setup_gitconfig () {
 EOF
 }
 
-
-setup_tmux () {
-        cat << 'EOF' > ~/.tmux.conf
+setup_tmux() {
+        cat <<'EOF' >~/.tmux.conf
 set-window-option -g mode-keys vi
 set -g prefix C-a
 set -g history-limit 10000
@@ -100,9 +93,8 @@ set -g status-right ''
 EOF
 }
 
-
-setup_bashrc () {
-        cat << 'EOF' > ~/.bashrc
+setup_bashrc() {
+        cat <<'EOF' >~/.bashrc
 # if not running interactively, don't do anything
 #
 [ -z "$PS1" ] && return
